9926ec422d3f27b0aa7bad887e4bbadb26925e94
IMS <gudwnsrh@gmail.com>
2020-03-10 02:33:46 +0900

    20.03.10 CollectionFrameWork Study

---
+++ src/ProgrammersBinarySearch2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ProgrammersBinarySearch2 {
+    public static void main(String args[]){
+        int x[] = {3};
+        int ky = 3;
+       int idx = Arrays.binarySearch(x,ky);
+    }
+}
