312814717272f5f4af1b4034cc836fe86a1a0ba8
IMS <gudwnsrh@gmail.com>
2020-03-11 00:17:51 +0900

    20.03.11 SortPractice

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,28 +1,29 @@
 public class InsertionSort {
-    public void sort(int[] A){
-        int size = A.length;
-        int temp = 0;
-        int j = 0;
-        for(int i = 1; i < size; i++){
-            temp = A[i];
-            for(j=i-1; j>=0 && temp<A[j]; j--){
-                A[j+1] = A[j];
+    public void sort(int[] x) {
+        int size = x.length;
+        int temp;
+        int j=0;
+        for (int i = 0; i < 4; i++) {
+            j = i;
+            while (x[j] > x[j+1]) {
+                temp = x[j];
+                x[j]=x[j+1];
+                x[j+1]=temp;
+                j--;
             }
-            A[j+1] = temp;
         }
     }
+
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         InsertionSort insertion = new InsertionSort();
 
-        int A[] = {66, 10, 1, 34, 5};
+        int a[] = {66, 10, 1, 34, 5,6};
 
-        insertion.sort(A);
-        for(int i = 0; i < A.length; i++){
-            System.out.println("A["+i+"] : " + A[i]);
-        }
+        insertion.sort(a);
     }
 
 
 }
+
