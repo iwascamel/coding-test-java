87924d7aebd973b91d6b282eef95ea9ec4ef7ca7
camel-man-ims <gudwnsrh@gmail.com>
2021-01-14 22:24:27 +0900

    21-01-04

---
+++ src/InflearnLecture/MeetingRoom2.java
@@ -0,0 +1,15 @@
+package InflearnLecture;
+
+/*
+문제 : interval 이 주어지고, 필요한 회의실의 갯수를 구하는 문제
+
+ex) 0-30분 회의 , 5-10분, 15-20분
+
+이렇게 주어지면 0-30분 회의실 하나, 5-10분 / 15-20분 회의실 하나씩 해서 총 2개가 필요
+ */
+
+public class MeetingRoom2 {
+    public static void main(String[] args) {
+
+    }
+}
