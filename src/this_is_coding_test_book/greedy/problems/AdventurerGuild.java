be18ca1f271e5313f3ae517ed41920178d6abcc8
camel-man-ims <gudwnsrh@gmail.com>
2021-02-12 00:44:44 +0900

    21-02-11 알고리즘

---
+++ src/this_is_coding_test_book/greedy/problems/AdventurerGuild.java
@@ -0,0 +1,29 @@
+package this_is_coding_test_book.greedy.problems;
+/*
+📌 311 page
+
+01. 모험가 길드
+
+ */
+
+import java.util.Arrays;
+
+public class AdventurerGuild {
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,2,2,5,3,2,2,1,1,2,3,3,2};
+
+        int solution = solution(arr);
+        System.out.println(solution);
+    }
+
+    private static int solution(int[] arr) {
+        Arrays.sort(arr);
+        int pointer = 0;
+        int count=0;
+        while(pointer< arr.length){
+            pointer += arr[pointer];
+            count++;
+        }
+        return count-1;
+    }
+}
