afd2964dd6c036c1f3f8d4aaca6ab66d2e82993d
camel-man-ims <gudwnsrh@gmail.com>
2021-05-14 23:35:40 +0900

    21-05-14 코딩인터뷰

--- src/ScratchPad.java
+++ src/ScratchPad.java
@@ -1,18 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class ScratchPad {
 
     public static void main(String[] args) {
-        List<Integer> li = new ArrayList<>();
 
-        li.add(10);
-        li.add(20);
-        li.add(30);
-        li.add(40);
-        li.add(50);
-
-        System.out.println(li.get(3));
     }
 }
 
