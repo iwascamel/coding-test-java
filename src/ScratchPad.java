cb018e8de16b7150ae3398865a7402c946c1bb41
camel-man-ims <gudwnsrh@gmail.com>
2021-05-29 01:28:25 +0900

    21-05-29 코딩인터뷰

--- src/ScratchPad.java
+++ src/ScratchPad.java
@@ -2,14 +2,7 @@
 
 public class ScratchPad {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int[] temp = arr;
 
-        temp[2]=100;
-
-        for(int i : arr){
-            System.out.print(i + " ");
-        }
     }
 }
 
