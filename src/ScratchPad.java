6b0951091683e301ad8485d6da29d749b935d0db
camel-man-ims <gudwnsrh@gmail.com>
2021-05-31 16:54:21 +0900

    21.05.26

--- src/ScratchPad.java
+++ src/ScratchPad.java
@@ -2,24 +2,26 @@ public class ScratchPad {
     public static void main(String[] args) {
 
         Dog a = new Dog("A");
-        Dog b = a;
+        Dog b = new Dog("B");
 
-        foo(a);
+        swap(a,b);
 
         System.out.println(a.name);
         System.out.println(b.name);
 
     }
+
+    static void swap(Dog a, Dog b){
+        Dog temp;
+        temp =a;
+        a=b;
+        b=temp;
+    }
     static class Dog{
         String name;
-
         Dog(String name){
             this.name=name;
         }
-    }
-
-    static void foo(Dog d){
-        d = new Dog("C");
     }
 }
 
