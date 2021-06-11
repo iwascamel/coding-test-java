c0d20187de48a0022e716dccd9f44974f1b42687
camel-man-ims <gudwnsrh@gmail.com>
2021-06-11 15:05:08 +0900

    21-06.11

--- src/ScratchPad.java
+++ src/ScratchPad.java
@@ -2,12 +2,17 @@
 
 public class ScratchPad {
     public static void main(String[] args) {
-        for(int j=0;j<20;j++){
-            for(int i=0;i<10;i=j){
-                System.out.println(i);
-            }
-        }
+        recursiveResult(15);
+    }
+    public static String recursiveString(int n){
+        if (n==1) return "1";
+        else return recursiveString(n-1)+n;
+    }
+    public static void recursiveResult(int n){
+        if(n==0) return ;
 
+        System.out.println(recursiveString(n));
+        recursiveResult(n-1);
     }
 }
 
